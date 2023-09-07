7bf69c31a484edce5e9c8d0d3cee4106f685d895
Eugene <93394578+EuGeNe-Las@users.noreply.github.com>
2023-09-07 14:53:42 +0300

    Добавьте файлы проекта.

---
+++ WEB_153501_Lasevich/Controllers/Home.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_153501_Lasevich.Controllers
+{
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+		
+	}
+}
---
+++ WEB_153501_Lasevich/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_153501_Lasevich.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
